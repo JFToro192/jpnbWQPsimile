2832a1ff0f9ac3ec6aee0076208a4a9ba29ae3cf
juanfernando.toro <juanfernando.toro@polimi.it>
2022-09-26 12:10:22 +0200

    Notebook Outlier Rejection and Sampling

--- setup/update-snap.sh
+++ setup/update-snap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539785/Update+SNAP+from+the+command+line
-#/usr/local/snap/bin/snap --nosplash --nogui --modules --refresh --update-all  2>&1 | while read -r line; do
+# /usr/local/snap/bin/snap --nosplash --nogui --modules --refresh --update-all  2>&1 | while read -r line; do
 /usr/local/snap/bin/snap --nosplash --nogui --modules --refresh --update org.esa.s3tbx.s3tbx.landsat.reader org.esa.s3tbx.s3tbx.c2rcc 2>&1 | while read -r line; do
     echo "$line"
     [ "$line" = "updates=0" ] && sleep 2 && pkill -TERM -f "snap/jre/bin/java"
